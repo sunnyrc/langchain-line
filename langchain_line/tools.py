bcad05d002f47cddcc8a3c3a99d821cf7f824449
sanchit <sanchit_sid@hotmail.com>
2026-03-16 15:52:02 +0700

    added more tools

--- langchain_line/tools.py
+++ langchain_line/tools.py
@@ -23,3 +23,33 @@ class LinePushMessage(BaseTool):
     def _run(self, user_id: str, message: str) -> str:
         """Push a message to a LINE user."""
         return self.channel_access_token.push_message(user_id, message)
+
+class LineGetProfile(BaseTool):
+    name: str = "line_get_profile"
+    description: str = "Get a LINE user's profile. Requires user_id."
+
+    channel_access_token: str
+
+    def _run(self, user_id: str) -> str:
+        """Get a LINE user's profile."""
+        return self.channel_access_token.get_profile(user_id)
+
+class LineGetGroupMembers(BaseTool):
+    name: str = "line_get_group_members"
+    description: str = "Get a list of LINE group members. Requires group_id."
+
+    channel_access_token: str
+
+    def _run(self, group_id: str) -> str:
+        """Get a list of LINE group members."""
+        return self.channel_access_token.get_group_members(group_id)
+
+class LineBroadcastMessage(BaseTool):
+    name: str = "line_broadcast_message"
+    description: str = "Send a message to all followers of the LINE bot. Requires message text."
+
+    channel_access_token: str
+
+    def _run(self, message: str) -> str:
+        """Broadcast a message to all LINE users."""
+        return self.channel_access_token.broadcast_message(message)
